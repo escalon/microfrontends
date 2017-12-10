de2bb2dbdc47ffe6c0150664c9062d186e659b06
dschulten <ds@escalon.de>
2017-12-10 14:31:03 +0100

    added order-service

---
+++ order-service/src/main/java/de/escalon/orderservice/OrderServiceApplication.java
@@ -0,0 +1,12 @@
+package de.escalon.orderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceApplication.class, args);
+	}
+}
