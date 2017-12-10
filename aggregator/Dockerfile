bb1c6641326f6349a26e6a563e71db020b8d8cb6
dschulten <ds@escalon.de>
2017-12-10 14:12:31 +0100

    initial commit

---
+++ aggregator/Dockerfile
@@ -0,0 +1,6 @@
+FROM java
+MAINTAINER Dietrich Schulten <ds@escalon.de>
+ENTRYPOINT ["/usr/bin/java", "-jar", "/usr/share/aggregator/aggregator.jar"]
+
+# ADD target/lib /usr/share/frontend/lib
+ADD target/aggregator.jar /usr/share/aggregator/aggregator.jar
---
+++ aggregator/src/main/java/de/escalon/aggregator/AggregatorApplication.java
@@ -0,0 +1,46 @@
+package de.escalon.aggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templatemode.StandardTemplateModeHandlers;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+import org.thymeleaf.templateresolver.UrlTemplateResolver;
+
+@SpringBootApplication
+public class AggregatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AggregatorApplication.class, args);
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.addTemplateResolver(templateResolver());
+        templateEngine.addTemplateResolver(urlTemplateResolver());
+        return templateEngine;
+    }
+
+    @Bean
+    public ITemplateResolver urlTemplateResolver() {
+        UrlTemplateResolver urlTemplateResolver = new UrlTemplateResolver();
+        urlTemplateResolver.setTemplateMode(StandardTemplateModeHandlers.LEGACYHTML5.getTemplateModeName());
+        return urlTemplateResolver;
+    }
+
+    @Bean
+    public ITemplateResolver templateResolver() {
+        SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+        resolver.setPrefix("classpath:templates/");
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode("HTML5");
+        resolver.setCharacterEncoding("UTF-8");
+        resolver.setCacheable(false);
+        return resolver;
+    }
+
+}
